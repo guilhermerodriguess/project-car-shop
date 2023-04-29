ba7fedf670cb40efe9f4f8f9013a99f7656c2620
Guilherme <grsguiga@gmail.com>
2023-04-28 21:34:26 -0300

    Requisito 3

---
+++ tests/unit/Services/car.test.ts
@@ -0,0 +1,67 @@
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import { expect } from 'chai';
+import Car from '../../../src/Domains/Car';
+import ICar from '../../../src/Interfaces/ICar';
+import CarService from '../../../src/Services/CarService';
+
+describe('Deveria criar um carro', function () {
+  it('Deveria criar um carro com SUCESSO', async function () {
+    // Arrange
+    const carInput: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    const carOutput: Car = new Car(
+      {
+        id: '644c522adeaf8379a9f9b694',
+        model: 'Marea',
+        year: 2002,
+        color: 'Black',
+        status: true,
+        buyValue: 15.99,
+        doorsQty: 4,
+        seatsQty: 5,
+      },
+    );
+    sinon.stub(Model, 'create').resolves(carOutput);
+
+    // Act
+    const service = new CarService();
+    const result = await service.createCar(carInput);
+
+    // Assert
+    expect(result).to.be.deep.equal(carOutput);
+  });
+  it('Deveria retornar todos os carros', async function () {
+    // Arrange
+    const carsOutput: ICar[] = [
+      {
+        id: '634852326b35b59438fbea2f',
+        model: 'Marea',
+        year: 2002,
+        color: 'Black',
+        status: true,
+        buyValue: 15.99,
+        doorsQty: 4,
+        seatsQty: 5,
+      },
+      {
+        id: '634852326b35b59438fbea31',
+        model: 'Tempra',
+        year: 1995,
+        color: 'Black',
+        buyValue: 39,
+        doorsQty: 2,
+        seatsQty: 5,
+      },
+    ];
+    // Act
+    // Assert
+  });
+});
